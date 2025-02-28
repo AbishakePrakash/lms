d05644888f1fc789ee1036f019cd8cc44d87d5ab
Abishake <abishakeprakash001@gmail.com>
2025-02-28 18:16:42 +0530

    OTP rebuild

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -105,13 +105,13 @@ export class UsersService {
 
       // Check whether Otp already exists
       async function checkDuplicateOtp() {
-        let newOtp: number;
-        let isExists: boolean;
-        do {
-          newOtp = otpGen();
-          isExists = await otpServiceX.checkOtp(newOtp);
-        } while (isExists);
-        return newOtp;
+        // let newOtp: number;
+        // let isExists: boolean;
+        // do {
+        //   newOtp = otpGen();
+        //   isExists = await otpServiceX.checkOtp(newOtp);
+        // } while (isExists);
+        return otpGen();
       }
 
       // Create and Save OTP
